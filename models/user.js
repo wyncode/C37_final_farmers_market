9e8f59e229036076b583c3d40c4f088cd58f06c2
Katie Breuil <kcbreuil@gmail.com>
2020-06-07 19:31:30 -0400

    encrypt passwords

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,6 +33,14 @@ const userSchema = new mongoose.Schema({
       }
     }
   }
+});
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 6);
+  }
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
