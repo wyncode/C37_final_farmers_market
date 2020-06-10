367b2e6343d6a8c3df376b0f1de1bc1b5f4154c9
Robert Garcia <robgarciaxyz@gmail.com>
2020-06-10 18:18:39 -0400

    set up and login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { AppContextProvider } from './context/AppContext';
-import ContextDemo from './components/ContextDemo';
-
-import './App.css';
+import Login from "./components/Login"
 
 const App = () => {
-  const [serverMessage, setServerMessage] = useState('');
-
-  const fetchDemoData = () => {
-    fetch('/api/demo')
-      .then((response) => response.json())
-      .then((data) => setServerMessage(data.message));
-  };
-
-  useEffect(fetchDemoData, []);
 
   return (
     <AppContextProvider>
-      <div id="demo">
-        <h3>Hello from client/src/App.js</h3>
-        <ContextDemo />
-        <h3>{serverMessage}</h3>
+      <div>
+        <h1>this is the app</h1>
+        <Login /> 
       </div>
     </AppContextProvider>
   );
