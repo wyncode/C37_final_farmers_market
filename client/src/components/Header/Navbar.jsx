55d29b3461888fe28228b75133add726b26a1abc
Katie Breuil <kcbreuil@gmail.com>
2020-06-12 19:18:47 -0400

    navbar progress

---
+++ client/src/components/Header/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppContext } from '../../context/AppContext';
+
+const Navbar = () => {
+  const { Home } = useContect(AppContext);
+  return (
+    <Navbar>
+      <Link></Link>
+    </Navbar>
+  );
+};
+
+export default Navbar;
---
+++ client/src/components/Header/Welcome.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+import "./Welcome.css" 
+import Logo from "."
