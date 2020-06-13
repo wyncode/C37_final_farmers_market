2b7d770df8c390bf4fb5f63666da0f8756b1dc04
Robert Garcia <robgarciaxyz@gmail.com>
2020-06-13 17:43:14 -0400

    produce page set up, added react-router-dom, react-bootstrap, bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { AppContextProvider } from './context/AppContext';
+import './App.css';
+import Produce from './pages/produce_page'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 // import Login from './components/Login';
 // import Logout from './components/Logout';
 // import Signup from './components/Signup';
@@ -8,10 +11,13 @@ import Home from './components/Home';
 const App = () => {
   return (
     <AppContextProvider>
+      <Router>
       <div>
         <h1>This is where we take over the world!!!</h1>
-        <Home />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/produce' component={Produce} />
       </div>
+      </Router>
     </AppContextProvider>
   );
 };
