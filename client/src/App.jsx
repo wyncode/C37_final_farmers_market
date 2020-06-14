d17163e6aa2ed091fc23b177718b8893bf66eef8
Katie Breuil <kcbreuil@gmail.com>
2020-06-14 00:25:48 -0400

    added 1 component on homepage and farm page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { AppContextProvider } from './context/AppContext';
-
 import './App.css';
-import Produce from './pages/produce_page'
-
-// import Login from './components/Login';
-// import Logout from './components/Logout';
-// import Signup from './components/Signup';
+import Produce from './pages/produce_page';
 import Navbar from './components/Header/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Account from './components/Account';
 import Farms from './components/Farms';
-import Produce from './components/Produce';
 
 const App = () => {
   return (
--- client/src/components/Farms.jsx
+++ client/src/components/Farms.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import logo from '../assets/images/logo.png';
+import './Farms.css';
 
 const Farms = () => {
-  return <h1>This is where the farms are gonna go!</h1>;
+  //some sort of onClick that will be attached the image,
+  // once the user clicks the image, it will change to a different div
+  return (
+    <div className="grid-container">
+      <div>
+        <img src={logo} alt="farmer" width="300" height="420" />
+      </div>
+      <div>
+        <h2>Joe's Organics</h2>
+      </div>
+      <div>
+        <img src={logo} alt="farmer" width="300" height="420" />
+      </div>
+      <div>
+        <h2>Verde Farm</h2>
+      </div>
+      <div>
+        <img src={logo} alt="farmer" width="300" height="420" />
+      </div>
+      <div>
+        <h2>Arrow Farm</h2>
+      </div>
+      <div>
+        <img src={logo} alt="farmer" width="300" height="420" />
+      </div>
+      <div>
+        <h2>On the Vine</h2>
+      </div>
+      <div>
+        <img src={logo} alt="farmer" width="300" height="420" />
+      </div>
+      <div>
+        <h2>Backyard Eggs</h2>
+      </div>
+      <div>
+        <img src={logo} alt="farmer" width="300" height="420" />
+      </div>
+      <div>
+        <h2>Finca Fresca</h2>
+      </div>
+      <div>
+        <img src={logo} alt="farmer" width="300" height="420" />
+      </div>
+      <div>
+        <h2>Sisters Farm</h2>
+      </div>
+      <div>
+        <img src={logo} alt="farmer" width="300" height="420" />
+      </div>
+      <div>
+        <h2>Tropical Farm</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Farms;
