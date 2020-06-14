00e050665accf857025e521cf46a8e67b6b46ba6
Katie Breuil <kcbreuil@gmail.com>
2020-06-14 11:07:29 -0400

    working flexbox on farm page

--- client/src/components/Farms.jsx
+++ client/src/components/Farms.jsx
@@ -6,55 +6,38 @@ const Farms = () => {
   //some sort of onClick that will be attached the image,
   // once the user clicks the image, it will change to a different div
   return (
-    <div className="grid-container">
-      <div>
-        <img src={logo} alt="farmer" width="300" height="420" />
-      </div>
-      <div>
-        <h2>Joe's Organics</h2>
-      </div>
-      <div>
-        <img src={logo} alt="farmer" width="300" height="420" />
-      </div>
-      <div>
-        <h2>Verde Farm</h2>
-      </div>
-      <div>
-        <img src={logo} alt="farmer" width="300" height="420" />
-      </div>
-      <div>
-        <h2>Arrow Farm</h2>
-      </div>
-      <div>
-        <img src={logo} alt="farmer" width="300" height="420" />
-      </div>
-      <div>
-        <h2>On the Vine</h2>
-      </div>
-      <div>
-        <img src={logo} alt="farmer" width="300" height="420" />
-      </div>
-      <div>
-        <h2>Backyard Eggs</h2>
-      </div>
-      <div>
-        <img src={logo} alt="farmer" width="300" height="420" />
-      </div>
-      <div>
-        <h2>Finca Fresca</h2>
-      </div>
-      <div>
-        <img src={logo} alt="farmer" width="300" height="420" />
-      </div>
-      <div>
-        <h2>Sisters Farm</h2>
-      </div>
-      <div>
-        <img src={logo} alt="farmer" width="300" height="420" />
-      </div>
-      <div>
-        <h2>Tropical Farm</h2>
-      </div>
+    <div className="grid">
+      <img src={logo} alt="farmer" width="300" height="420" />
+
+      <h2 className="farmdiv">Joe's Organics</h2>
+
+      <img src={logo} alt="farmer" width="300" height="420" />
+
+      <h2 className="farmdiv">Verde Farm</h2>
+
+      <img src={logo} alt="farmer" width="300" height="420" />
+
+      <h2>Arrow Farm</h2>
+
+      <img src={logo} alt="farmer" width="300" height="420" />
+
+      <h2>On the Vine</h2>
+
+      <img src={logo} alt="farmer" width="300" height="420" />
+
+      <h2>Backyard Eggs</h2>
+
+      <img src={logo} alt="farmer" width="300" height="420" />
+
+      <h2>Finca Fresca</h2>
+
+      <img src={logo} alt="farmer" width="300" height="420" />
+
+      <h2>Sisters Farm</h2>
+
+      <img src={logo} alt="farmer" width="300" height="420" />
+
+      <h2>Tropical Farm</h2>
     </div>
   );
 };
