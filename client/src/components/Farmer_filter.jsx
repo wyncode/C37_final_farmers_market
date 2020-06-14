a1fc74e1aa4128c37b7a51cb354bb19f544d4b4e
Robert Garcia <robgarciaxyz@gmail.com>
2020-06-14 15:05:23 -0400

    just to be on same page

--- client/src/components/Farmer_filter.jsx
+++ client/src/components/Farmer_filter.jsx
@@ -3,24 +3,31 @@ import axios from 'axios'
 
 const Farmer_Filter = () => {
 
-    const [farmers, setFarmers] = useState('')
+    const [farmers, setFarmers] = useState([])
 
     useEffect(() => {
+
         const farmProduce = async () => {
         await axios({
             method: 'GET',
-            url: `/products`
+            url: `/stores`
         })
-        //.then (({data}) => console.log(data))
+
         .then (({data}) => {
-            console.log(data.farmerStore.storeName)
+            setFarmers(data)
         })
         .catch(e => console.log(e.message.toString()))
     }
     farmProduce();
-    }, [farmers])
+    }, [])
+
+    const farmerList = farmers.map( farmer => farmer.storeName)
+
     return (
+        <div>
         <h1>Farmers</h1>
+        <h2>{farmerList}</h2>
+        </div>
     )
 }
 
