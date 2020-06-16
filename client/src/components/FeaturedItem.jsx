53d1bd5b3d9e7426bcf15e282fcf36353d758d6e
Katie Breuil <kcbreuil@gmail.com>
2020-06-15 22:20:57 -0400

    removed farm container comp

---
+++ client/src/components/FeaturedItem.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const FeaturedItems = () => {
+  const { produceList } = useContext(AppContext);
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default FeaturedItems;
