dfded5cc9f05a5568ab5ad13a594e5ace9fc7d84
Katie Breuil <kcbreuil@gmail.com>
2020-06-15 21:51:52 -0400

    refactored app context

--- client/src/components/FarmComponents/FarmContainer.jsx
+++ client/src/components/FarmComponents/FarmContainer.jsx
@@ -1,16 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../Farms.css';
 
-const FarmContainer = ({children}) => {
-  const [expanded, setExpanded] = useState(false);
-
-  //some sort of onClick that will be attached the image,
-  // once the user clicks the image, it will change to a different div
-  return (
-    <div className="farm-container">
-      {children}
-    </div>
-  );
+const FarmContainer = ({ children }) => {
+  return <div className="farm-container">{children}</div>;
 };
 
 export default FarmContainer;
--- client/src/components/FarmerFilter.jsx
+++ client/src/components/FarmerFilter.jsx
@@ -1,80 +1,43 @@
-import React,{ useState, useEffect} from 'react'
-import axios from 'axios'
+import React from 'react';
 
-const FarmerFilter = ({produceList, setDisplayedList, chosenStore, setChosenStore, farmers, setFarmers}) => {
+const FarmerFilter = ({
+  farmers,
+  setSearchTerm,
+  setChosenStore,
+  searchTerm
+}) => {
+  return (
+    <div>
+      <h1>Farmers</h1>
+      <p
+        onClick={() => {
+          setChosenStore('');
+          setSearchTerm('');
+        }}
+      >
+        All Stores
+      </p>
+      {farmers &&
+        farmers.map((farmer) => {
+          return (
+            <p
+              key={farmer._id}
+              onClick={() => {
+                setChosenStore(farmer._id);
+                setSearchTerm('');
+              }}
+            >
+              {farmer.storeName}
+            </p>
+          );
+        })}
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </div>
+  );
+};
 
-    
-    const [searchTerm, setSearchTerm] = useState("")
-    
-
-    useEffect(() => {
-
-        const farmProduce = async () => {
-        await axios({
-            method: 'GET',
-            url: `/stores`
-        })
-
-        .then (({data}) => {
-            setFarmers(data)
-        })
-        .catch(e => console.log(e.message.toString()))
-    }
-    farmProduce();
-    }, [setFarmers])
-
-    useEffect(()=>{
-        if (chosenStore === "") return setDisplayedList(produceList)
-        const filteredList = produceList.filter(product => {
-            return(
-                product.farmerStore === chosenStore
-            )
-        })
-        setDisplayedList(filteredList)
-    }, [chosenStore, produceList, setDisplayedList])
-
-    useEffect(()=>{        
-        if (chosenStore){
-            const filteredList = produceList.filter(product => {
-                return(
-                    product.farmerStore === chosenStore 
-                    &&
-                    product.name.toLowerCase().includes(searchTerm.toLowerCase())
-                )
-            })    
-            setDisplayedList(filteredList)
-        } else {
-            const filteredList = produceList.filter(product => {
-                return(
-                    product.name.toLowerCase().includes(searchTerm.toLowerCase())
-                )
-            })    
-            setDisplayedList(filteredList)
-        }
-    },[searchTerm, chosenStore, produceList, setDisplayedList])
-
-    return (
-        <div>
-        <h1>Farmers</h1>
-        <p onClick={() => {
-                        setChosenStore("") 
-                        setSearchTerm("")
-                    }}>All Stores</p>
-        {
-            farmers && farmers.map(farmer => {
-                return(
-                    <p key={farmer._id} onClick={() => {
-                        setChosenStore(farmer._id) 
-                        setSearchTerm("")
-                    }}>
-                        {farmer.storeName}
-                    </p>    
-                )
-            })
-        }
-        <input type="text" value = {searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
-        </div>
-    )
-}
-
-export default FarmerFilter
+export default FarmerFilter;
