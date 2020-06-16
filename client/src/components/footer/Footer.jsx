bfcc08bb0b724ac5312f91efde4a0b386c2d743c
Lazaro <lquin008@gmail.com>
2020-06-16 15:32:22 -0400

    cleaned up matthews work

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,37 +4,31 @@ import '../footer/footer.css';
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-content">
-        <div className="footer-section socials">
-          <a>
-            <i class="fab fa-instagram"></i>
-          </a>
-
-          <a>
-            <i class="fab fa-facebook-f"></i>
-          </a>
-
-          <a>
-            <i class="fab fa-twitter"></i>
-          </a>
-          <ul>
+        <div className="footer-social footer-section">
             <a>
-              <li>Help</li>
+              <i class="fab fa-instagram"></i>
             </a>
+
             <a>
-              <li>Payment Options</li>
+              <i class="fab fa-facebook-f"></i>
             </a>
+
+            <a>
+              <i class="fab fa-twitter"></i>
+            </a>
+          </div>
+          <ul className="footer-links footer-section">
+            <li>
+              <a>Help</a>
+            </li>
+            <li>
+              <a>Payment Options</a>
+            </li>
           </ul>
-        </div>
-
-        <div className="footerAddress">
-            <p>8305 Biscyane Rd</p>
-            <p>Miami, FL 33106</p>
-        </div>
-        <div className="footer-section links"></div>
-
-        <div className="footer-section address"></div>
-      </div>
+        <ul className="footer-section">
+            <li>8305 Biscyane Rd</li>
+            <li>Miami, FL 33106</li>
+        </ul>
     </div>
   );
 };
