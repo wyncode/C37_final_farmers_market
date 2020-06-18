20fc6dcf833559656ca62bcaa9a380fd8d44d97a
Katie Breuil <kcbreuil@gmail.com>
2020-06-18 15:17:14 -0400

    minor changes to matthews code and navbar

--- client/src/components/HowWorks.jsx
+++ client/src/components/HowWorks.jsx
@@ -5,40 +5,18 @@ import Farmer from '../assets/images/farmer.svg';
 import Utensils from '../assets/images/utensils.svg';
 
 const HowWorks = () => {
-    return(
-        
-<div className='theBox'>
-    <h2 className="howTitle">How it works</h2>
-    <img src={Farmer} className="howImg">
-    </img>
-    <img src={Avocado} className="howImg">
+  return (
+    <div className="theBox">
+      <h2 className="howTitle">How it works</h2>
+      <img src={Farmer} className="howImg"></img>
+      <img src={Avocado} className="howImg"></img>
+      <img src={Utensils} className="howImg"></img>
 
-</img>
-    <img src={Utensils} className="howImg">
-    </img>
+      <button type="button" className="howButton">
+        See Details
+      </button>
+    </div>
+  );
+};
 
-    <button type="button" className="howButton">
-See Details
-    </button>
-</div>
-
-    
-
-    )
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-export default HowWorks
+export default HowWorks;
