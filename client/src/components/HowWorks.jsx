03477bc37cb020613ed427b10dd6cd9341791263
mxnageyt <mxnageyt@gmail.com>
2020-06-18 12:09:08 -0400

    adding howto and community

---
+++ client/src/components/HowWorks.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './HowWorks.css';
+import Avocado from '../assets/images/avocado.svg';
+import Farmer from '../assets/images/farmer.svg';
+import Utensils from '../assets/images/utensils.svg';
+
+const HowWorks = () => {
+    return(
+        
+<div className='theBox'>
+    <h2 className="howTitle">How it works</h2>
+    <img src={Farmer} className="howImg">
+    </img>
+    <img src={Avocado} className="howImg">
+
+</img>
+    <img src={Utensils} className="howImg">
+    </img>
+
+    <button type="button" className="howButton">
+See Details
+    </button>
+</div>
+
+    
+
+    )
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export default HowWorks
