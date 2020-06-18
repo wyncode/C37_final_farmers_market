0e78089a6786e61d77d8330f913cab202f4c1ef6
Robert Garcia <robgarciaxyz@gmail.com>
2020-06-18 19:53:01 -0400

    working funct on remove button

--- client/src/components/produce/IdProductPage.jsx
+++ client/src/components/produce/IdProductPage.jsx
@@ -31,6 +31,33 @@ const IdProduct = () => {
     });
   };
 
+    const decrementUpdateCart = (produce) => {
+    let currentItemInCart = shoppingCart[produce._id];
+  
+
+    if (currentItemInCart.count === 1) { 
+      
+      currentItemInCart= ({...shoppingCart,[produce._id]: { count: 0, produce }})
+        delete currentItemInCart[produce._id]
+        return setShoppingCart(currentItemInCart)
+      }
+  if (currentItemInCart) {
+        return setShoppingCart({
+          ...shoppingCart,
+          [produce._id]: { count: currentItemInCart.count - 1, produce }
+    })
+  }
+  }
+
+  const currentItemCart = (product) => {
+    const currentItemInCart = shoppingCart[product._id];
+    if(currentItemInCart) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   const prodImg =
     'https://pluspng.com/img-png/vegetable-png-hd-vegetable-png-transparent-image-1799.png';
 
@@ -53,6 +80,7 @@ const IdProduct = () => {
           <button onClick={() => handleUpdateCart(product)}>
                       Add to Cart
                     </button>
+                    {currentItemCart(product) ? <button onClick={() => decrementUpdateCart(product)}>Remove</button> : null}
           <h3>{product && product.description}</h3>
           <h3>Contributing Farm</h3>
           <h3
